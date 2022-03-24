f6cabc3c5c2a7216c033e24398af4ab3fb9f5879
gegota <gegota@yandex.ru>
2022-03-24 21:37:15 +0300

    start project

--- script.js
+++
@@ -1,9 +0,0 @@
-
-const div = document.getElementById('GitHub')
-
-const logged = () => {
-  console.log(div. textContent);
-  console.log('commits');
-}
-console.log('personAge', personAge);
-// asdasd
