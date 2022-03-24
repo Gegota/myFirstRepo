89cd62c5511de52798ceeae8f2d9ba5eb343c001
gegota <gegota@yandex.ru>
2022-03-24 23:13:57 +0300

    добавил ветку мастера и дев

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-alert("Привет");
-console.log(" Привет");  
-
 let title;
 let screens;
 let screenPrice;
 let rollback;
 let fullPrice;
 let adaptive;
+
+alert("Привет");
+console.log(" Привет");  
